e3de0d7d9549d82e7941b67331e352eb26082e90
Brandon Ling <79050338+bzzling@users.noreply.github.com>
2024-10-06 21:36:45 -0400

    pretty much complete

--- src/header/header.component.ts
+++ src/header/header.component.ts
@@ -1,12 +1,42 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './header.component.html',
-  styleUrl: './header.component.css'
+  styleUrls: ['./header.component.css']
 })
 export class HeaderComponent {
+  private lastScrollTop = 0;
 
+  @HostListener('window:scroll', [])
+  onWindowScroll() {
+    const navbar = document.getElementById('navbar');
+    const st = window.pageYOffset || document.documentElement.scrollTop;
+
+    if (st > this.lastScrollTop) {
+      // Downscroll
+      if (navbar) {
+        navbar.style.transform = 'translateY(-100%)'; // Move navbar out of view
+      }
+    } else {
+      // Upscroll
+      if (navbar) {
+        navbar.style.transform = 'translateY(0)'; // Move navbar into view
+      }
+    }
+    this.lastScrollTop = st <= 0 ? 0 : st; // For Mobile or negative scrolling
+  }
+
+  scrollToSection(event: Event, sectionId: string) {
+    event.preventDefault();
+    const section = document.getElementById(sectionId);
+    if (section) {
+      const yOffset = -80;
+      const y = section.getBoundingClientRect().top + window.pageYOffset + yOffset;
+      window.scrollTo({ top: y, behavior: 'smooth' });
+    }
+  }
 }
